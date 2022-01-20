8a1bf9d81907f3e0d235cd70de8a509ad3a37a51
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-01-20 21:35:06 +0100

    Initial import

---
+++ Avalonia.NETCoreApp1/Custom.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Numerics;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Avalonia.NETCoreApp1
+{
+    public class Custom : Decorator
+    {
+        public static readonly StyledProperty<double> YawProperty = 
+            AvaloniaProperty.Register<Custom, double>(nameof(Yaw));
+
+        public static readonly StyledProperty<double> PitchProperty = 
+            AvaloniaProperty.Register<Custom, double>(nameof(Pitch));
+
+        public static readonly StyledProperty<double> RollProperty = 
+            AvaloniaProperty.Register<Custom, double>(nameof(Roll));
+
+        public double Yaw
+        {
+            get => GetValue(YawProperty);
+            set => SetValue(YawProperty, value);
+        }
+
+        public double Pitch
+        {
+            get => GetValue(PitchProperty);
+            set => SetValue(PitchProperty, value);
+        }
+
+        public double Roll
+        {
+            get => GetValue(RollProperty);
+            set => SetValue(RollProperty, value);
+        }
+
+        protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+        {
+            SetChildTransform();
+
+            base.OnAttachedToVisualTree(e);
+        }
+
+        protected override void OnPropertyChanged<T>(AvaloniaPropertyChangedEventArgs<T> change)
+        {
+            if (change.Property == YawProperty
+                || change.Property == PitchProperty
+                || change.Property == RollProperty)
+            {
+                SetChildTransform();
+                //InvalidateVisual();
+            }
+            
+            base.OnPropertyChanged(change);
+        }
+
+        private void SetChildTransform()
+        {
+            if (Child is { })
+            {
+                var matrix = CrateTransform(Yaw, Pitch, Roll);
+                Child.RenderTransform = new MatrixTransform(matrix);
+            }
+        }
+
+        private static Matrix CrateTransform(double yaw, double pitch, double roll)
+        {
+            static double DegreeToRadian(double degrees) => Math.PI * degrees / 180.0;
+            var yawRadians = (float)DegreeToRadian(yaw);
+            var pitchRadians = (float)DegreeToRadian(pitch);
+            var rollRadians = (float)DegreeToRadian(roll);
+
+            var q = Quaternion.CreateFromYawPitchRoll(yawRadians, pitchRadians, rollRadians);
+            var m = Matrix4x4.CreateFromQuaternion(q);
+
+            return new Matrix(m.M11, m.M12, m.M21, m.M22, m.M31, m.M32);
+        }
+
+        public override void Render(DrawingContext context)
+        {
+            var op = new CustomDrawOp(Yaw, Pitch, Roll, Bounds);
+
+            //context.Custom(op);
+            
+            base.Render(context);
+        }
+    }
+}
---
+++ Avalonia.NETCoreApp1/Demo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using SkiaSharp;
+
+namespace Avalonia.NETCoreApp1
+{
+    public class Demo
+    {
+        public static void Draw(SKCanvas canvas, SKRect bounds, double yaw, double pitch, double roll)
+        {
+            canvas.Save();
+
+            canvas.Translate(bounds.Width / 2, bounds.Height / 2);
+
+            var rect = new SKRect(-100, -100, 100, 100);
+
+            var skMatrix = CrateTransform(yaw, pitch, roll);
+
+            canvas.Concat(ref skMatrix);
+
+            var paint = new SKPaint
+            {
+                Color = SKColors.Red,
+                Style = SKPaintStyle.Fill,
+                IsAntialias = true
+            };
+
+            canvas.DrawRoundRect(rect, 25, 25, paint);
+
+            canvas.Restore();
+        }
+
+        private static SKMatrix CrateTransform(double yaw, double pitch, double roll)
+        {
+            var yawRadians = (float)DegreeToRadian(yaw);
+            var pitchRadians = (float)DegreeToRadian(pitch);
+            var rollRadians = (float)DegreeToRadian(roll);
+
+            var q = Quaternion.CreateFromYawPitchRoll(yawRadians, pitchRadians, rollRadians);
+            var m = Matrix4x4.CreateFromQuaternion(q);
+
+            var skMatrix44 = ToSKMatrix44(ref m);
+            var skMatrix = skMatrix44.Matrix;
+
+            return skMatrix;
+        }
+
+        public static void Save(string path, Action<SKCanvas> draw, int width, int height)
+        {
+            var skImageInfo = new SKImageInfo(width, height, SKImageInfo.PlatformColorType, SKAlphaType.Unpremul,  SKColorSpace.CreateSrgb());
+            var skBitmap = new SKBitmap(skImageInfo);
+            using var skCanvas = new SKCanvas(skBitmap);
+            draw(skCanvas);
+            using var skImage = SKImage.FromBitmap(skBitmap);
+            using var skData = skImage.Encode(SKEncodedImageFormat.Png, 100);
+            if (skData is null) return;
+            using var stream = File.OpenWrite(path);
+            skData.SaveTo(stream);
+        }
+
+        public static SKMatrix44 ToSKMatrix44(ref Matrix4x4 m)
+        {
+            var rowMajor = new float[] {
+                m.M11, m.M12, m.M13, m.M14,
+                m.M21, m.M22, m.M23, m.M24,
+                m.M31, m.M32, m.M33, m.M34,
+                m.M41, m.M42, m.M43, m.M44
+            };
+            return SKMatrix44.FromRowMajor(rowMajor);
+        }
+
+        public static double DegreeToRadian(double degrees)
+        {
+            return Math.PI * degrees / 180.0;
+        }
+
+        public static double RadianToDegree(double radians)
+        {
+            return radians * (180.0 / Math.PI);
+        }
+    }
+}
