ee58c2d96dacf6b1c6ccb63a7ce8b3482ac3bbf0
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-02-04 12:48:43 +0100

    Add main view

---
+++ YawPitchRollTransform.Base/MainView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace YawPitchRollTransform;
+
+public class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
